dfefa61e7d1ab7a63a73006b4ff167c1c8334755
hector-torres2386 <hector.torres2386@gmail.com>
2022-11-24 19:42:46 -0600

    blah1

---
+++ Desktop/memos1/src/app/component/memo/memo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-memo',
+  templateUrl: './memo.component.html',
+  styleUrls: ['./memo.component.css']
+})
+export class MemoComponent {
+
+}
